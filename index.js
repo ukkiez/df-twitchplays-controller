a5e0f5979c1e0efe45a893fce5bc146d22296dff
Yannick Kinsbergen <ykinsbergen@gofilex.nl>
2022-05-20 20:47:40 +0200

    export parse and addKillSwitch functions

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const { GlobalKeyboardListener } = require( "node-global-key-listener" );
 
-const v = new GlobalKeyboardListener();
-v.addListener( function( e, down ) {
-  if ( down[ "0" ] ) {
-    // kill the process
-    process.exit( 0 );
-  }
-} );
+const globalKeyboardListener = new GlobalKeyboardListener();
+const addKillSwitch = ( key ) => {
+  globalKeyboardListener.addListener( function( e, down ) {
+    if ( down[ key ] ) {
+      // kill the process
+      process.exit( 0 );
+    }
+  } );
+};
 
 const {
   parse,
 } = require( "./control.js" );
 
-module.exports = parse;
+module.exports = { parse, addKillSwitch };
